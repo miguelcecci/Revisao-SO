2590fac74f43661624dd3cadf175eea998c59ed9
miguel <miguelnetojet@hotmail.com>
2016-11-12 00:46:09 -0200

    adicionando exercicio 3

---
+++ cod3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include <sys/wait.h>
+
+int main(void){
+  int status;
+  while(1){
+    //execl( "/bin/ls","ls",0 ); // leia_comandos( comando, parametros );
+    if(fork() != 0){
+      //pai
+      waitpid( -1, &status, 0); //espera o processo filho terminar
+    }else{
+      //filho
+      execve( "/bin/ls","ls", 0);
+    }
+  }
+}
+///////////////////------ATENÇÃO, EM OBRAS--------////////////////////////
