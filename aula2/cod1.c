fbbcd45b2c76e1d618a7933620784cf65c5eac93
miguel <miguelnetojet@hotmail.com>
2016-11-16 22:28:48 -0200

    adicionada mais algumas anotacoes na aula 2

--- aula2/cod1.c
+++ aula2/cod1.c
@@ -3,23 +3,24 @@
 #include <stdlib.h>
 
 int main(void){
-  int pfd[2];
-  pipe(pfd);
+  int pfd[2]; //variaveis do filedescriptor
+  pipe(pfd); //cria um pipe na variavel pfd
 
   switch (fork()) {
-    case -1: exit(1); break;
+    case -1: exit(1); break; //erro, termina a execuçao do programa
 
-    case 0: close(0);
-    dup(pfd[0]);
-    close(pfd[1]);
+    case 0: close(0); //o processo filho ira executar essa parte do programa
+    dup(pfd[0]);//cria um pfd[3]
+    close(pfd[1]); //fecha o filedescriptor
     close(pfd[0]);
     execl("/usr/bin/wc","wc",NULL);
-    break;
+    break; //termina a execuçao
   }
   switch (fork()) {
-    case -1: exit(1); break;
+    case -1: exit(1); break; //erro, termina a execuçao do programa
 
-    case 0: close(1);
+    //o processo filho ira executar essa parte do programa
+    case 0: close(1);  //
     dup(pfd[1]);
     close(pfd[0]);
     close(pfd[1]);
